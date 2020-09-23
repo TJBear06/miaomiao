578a720c8afd26d7e9e091ce35fc60b33662705d
TJBear06 <tjbear006@gmail.com>
2020-09-23 19:16:10 +0800

    add createComponents

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="main">
+        <Header title="我的喵喵"/>
+        <div id="content">
+            <Login/>
+        </div>
+        <TabBar/>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Header from '@/components/Header'
+import TabBar from '@/components/TabBar'
+import Login from '@/components/Login'
+
+export default Vue.extend({
+    name: 'Mine',
+    components: {
+        Header,
+        TabBar,
+        Login
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
